644060f175171d5f3fd2b394231942b88bf0adb6
Jasminoza <testbot91@gmail.com>
2022-06-28 09:50:42 +0300

    refactored

--- src/main/java/controller/SkillController.java
+++ src/main/java/controller/SkillController.java
@@ -2,33 +2,21 @@ package controller;
 
 import model.Skill;
 import repository.SkillRepository;
-//import repository.gson.GsonSkillRepositoryImpl;
 import repository.mysql.MySQLSkillRepository;
-import utils.ResultSetConverter;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class SkillController {
     private final SkillRepository skillRepository = new MySQLSkillRepository();
 
-
     public Skill createSkill(String name) {
         Skill skill = new Skill();
         skill.setName(name);
-        try {
-            return ResultSetConverter.convertToSkill(skillRepository.create(skill));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return skillRepository.create(skill);
     }
 
     public List<Skill> getAllSkills() {
-        try {
-            return ResultSetConverter.convertToSkillsList(skillRepository.getAll());
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return skillRepository.getAll();
     }
 
     public void deleteSkill(Long id) {
@@ -41,10 +29,6 @@ public class SkillController {
     }
 
     public Skill getById(Long id) {
-        try {
-            return ResultSetConverter.convertToSkill(skillRepository.getById(id));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return skillRepository.getById(id);
     }
 }
