bf14c954ab086be6173d8d3a6b593d103d2efb18
Jasminoza <testbot91@gmail.com>
2022-06-28 09:53:44 +0300

    refactored

--- src/main/java/utils/ConnectionToMySQL.java
+++ src/main/java/utils/ConnectionToMySQL.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionToMySQL extends ConnectionImpl {;
+public class ConnectionToMySQL extends ConnectionImpl {
     private static ConnectionToMySQL connectionToMySQL;
 
     private ConnectionToMySQL() {
