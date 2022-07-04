2b8828c2b36bae38ceca756eb31b29d20c419ac5
Jasminoza <testbot91@gmail.com>
2022-07-04 15:34:10 +0300

    liquibase migration implemented

--- src/main/resources/liquibase/crudConsoleApp/changelog/changeset/create-table-skills.sql
+++
@@ -1,6 +0,0 @@
---changeset author:YolkinNV
-create TABLE `skills` (
-    `id` int NOT NULL AUTO_INCREMENT,
-	`name` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`)
-);
--- src/main/resources/liquibase/crudConsoleApp/changelog/changeset/create-table-specialties.sql
+++
@@ -1,6 +0,0 @@
---changeset author:YolkinNV
-create TABLE `specialties` (
-    `id` int NOT NULL AUTO_INCREMENT,
-	`name` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`)
-);
